6b3c989f4740468495ef85f70c6922b4faa2716b
Adam Dick <adam.c.dick@gmail.com>
2019-06-17 12:26:39 -0400

    added data gathering for product information

--- Source/farfetch.py
+++ Source/farfetch.py
@@ -17,7 +17,7 @@ class Farfetch():
         self.product_collection = self.database['product_details']
         self.recommender_system = self.init_recommender_system()
         self.driver             = None
-        
+    
     ################################################################################
     # DATA GATHERING: CUSTOMER REVIEWS
     ################################################################################
@@ -141,3 +141,133 @@ class Farfetch():
             json.dump(list(self.review_collection.find({}, {'_id': 0})), f)
         
         return self.review_collection
+    
+    ################################################################################
+    # DATA GATHERING: PRODUCT DETAILS
+    ################################################################################
+    
+    # deletes all documents from the collection of product details
+    def clear_product_collection(self):
+        self.product_collection.delete_many({})
+        
+        return self.product_collection
+    
+    # parses a single product from HTML soup for the URL
+    def parse_product(self, url, soup):
+        
+        # create the product details to be returned
+        product_details = {}
+        
+        # matching URL
+        product_details['URL']          = url
+        
+        # sold out, original price, discount, on sale
+        if soup.find('button', {"data-tstid": "letMeNowWhenBack"}):
+            product_details['Out of Stock'] = True
+            self.product_collection.insert_one(product_details)
+            return product_details
+        
+        elif soup.find('strong', {"data-tstid": "priceInfo-original"}):
+            product_details['Original'] = soup.find('strong', {"data-tstid": "priceInfo-original"}).get_text().strip()
+            
+        elif soup.find('del', {"data-tstid": "priceInfo-original"}):
+            product_details['Original'] = soup.find('del', {"data-tstid": "priceInfo-original"}).get_text().strip()
+            
+            if soup.find('span', {"data-tstid": "priceInfo-discount"}):
+                product_details['Discount'] = soup.find('span', {"data-tstid": "priceInfo-discount"}).get_text().strip()
+                
+            product_details['On Sale'] = soup.find('strong', {"data-tstid": "priceInfo-onsale"}).get_text().strip()
+            
+        else:
+            product_details['Out of Stock'] = True
+            self.product_collection.insert_one(product_details)
+            return product_details
+        
+        # designer and product information
+        top                         = soup.find('div', class_ = '_638126')
+        product_details['Designer'] = top.find('a').get_text().strip()
+        product_details['Product']  = top.find('span', class_ = '_077245').get_text().strip()
+        
+        # gender, category, subcategory
+        banner = soup.find('ol', class_ = 'e6f19e')
+        
+        if banner.find('a', {"data-type": "gender"}):
+            product_details['Gender'] = banner.find('a', {"data-type": "gender"}).get_text().strip()
+        
+        if banner.find('a', {"data-type": "category"}):
+            product_details['Category'] = banner.find('a', {"data-type": "category"}).get_text().strip()
+            
+        if banner.find('a', {"data-type": "subcategory"}):
+            product_details['Subcategory'] = banner.find('a', {"data-type": "subcategory"}).get_text().strip()
+        
+        # style, color, made in
+        if soup.find('p', {"data-tstid": "designerStyleId"}):
+            product_details['Style'] = soup.find('p', {"data-tstid": "designerStyleId"}).get_text().split(':')[1].strip()
+            
+        if soup.find('p', {"data-tstid": "designerColor"}):
+            product_details['Color'] = soup.find('p', {"data-tstid": "designerColor"}).get_text().split(':')[1].strip()
+            
+        if soup.find('p', {"data-tstid": "madeIn"}):
+            product_details['Made In'] = soup.find('p', {"data-tstid": "madeIn"}).get_text().strip()
+        
+        # insert product details into Mongo collection
+        self.product_collection.insert_one(product_details)
+        
+        return product_details
+    
+    # collects the product details that have been rated from the site
+    def parse_site_products(self, start_index):
+        utility_matrix, users, items = self.get_utility_matrix()
+        headers                      = {'user-agent': '{} {} {}'.format(
+                                                      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6)',
+                                                      'AppleWebKit/537.36 (KHTML, like Gecko)',
+                                                      'Chrome/53.0.2785.143 Safari/537.36')}
+        
+        count = start_index
+        # collects product details for all unique products in utility matrix
+        for url in items['Item_Link'].iloc[start_index:]:
+            status = -1
+            
+            # continues to request URL
+            while status != 200:
+                print(str(count) + ': ' + url)
+                
+                try:
+                    page   = requests.get(url, headers = headers, timeout = 5)
+                    status = page.status_code
+                    
+                except:
+                    time.sleep(10)
+                    
+            count += 1
+            soup   = BS(page.content, 'html.parser')
+            self.parse_product(url, soup)
+            time.sleep(3)
+            
+        return self.product_collection
+    
+    # saves the documents in the product collection to a json file
+    def save_products_to_json(self, path):
+        with open(path, 'w') as f:
+            json.dump(list(self.product_collection.find({}, {'_id': 0})), f)
+        
+        return self.product_collection
+    
+    # check if product is stocked out
+    def check_stock_out(self, url, soup):
+        stock_out = True
+        
+        # sold out, original price, discount, on sale
+        if soup.find('button', {"data-tstid": "letMeNowWhenBack"}):
+            stock_out = False
+        
+        elif soup.find('strong', {"data-tstid": "priceInfo-original"}):
+            stock_out = False
+            
+        elif soup.find('del', {"data-tstid": "priceInfo-original"}):
+            stock_out = False
+            
+        else:
+            pass
+        
+        return product_details
